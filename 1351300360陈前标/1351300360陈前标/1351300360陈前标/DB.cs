f5ccde8d1d00ba688424443c5189edac780c0dc0
chenqb000 <418801608@qq.com>
2016-12-25 21:49:47 +0800

    创建数据库文件

---
+++ 1351300360陈前标/1351300360陈前标/1351300360陈前标/DB.cs
@@ -0,0 +1,20 @@
+﻿namespace _1351300360陈前标
+{
+    using System;
+    using System.Data.Entity;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Linq;
+
+    public partial class DB : DbContext
+    {
+        public DB()
+            : base("name=DB")
+        {
+        }
+
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+        }
+    }
+}
