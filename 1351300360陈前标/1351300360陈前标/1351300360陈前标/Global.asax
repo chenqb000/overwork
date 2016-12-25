6c308b9e193f091ea9e5afb7305663f3fb7f5036
chenqb000 <418801608@qq.com>
2016-12-25 21:48:09 +0800

    创建MVC项目

---
+++ 1351300360陈前标/1351300360陈前标/1351300360陈前标/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_1351300360陈前标.MvcApplication" Language="C#" %>
---
+++ 1351300360陈前标/1351300360陈前标/1351300360陈前标/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_1351300360陈前标.Startup))]
+namespace _1351300360陈前标
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
